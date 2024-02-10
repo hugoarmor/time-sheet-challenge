3ddcb26d80c99431679db951ce4ef54016b8e457
hugoarmor <hugoarmoreira@hotmail.com>
2024-02-10 18:15:18 -0300

    update: finish post.batidas logic

--- src/routes/post.batidas.ts
+++ src/routes/post.batidas.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import { Router, Request, Response } from "express";
 import * as date from "date-fns";
-import { PostBatidasSerializer } from "../serializers/post_batidas";
+import { PostBatidasSerializer } from "../serializers/post-batidas";
 
 const router = Router();
 
@@ -23,6 +23,33 @@ router.post("/v1/batidas", async (req: Request, res: Response) => {
   const dateObject = date.parseISO(momento);
   const yearMonth = date.format(dateObject, "yyyy-MM");
 
+  const existingPunch = await db.timePunch.findFirst({
+    where: {
+      moment: dateObject,
+    },
+  });
+
+  if(!!existingPunch) {
+    return res.status(409).json({
+      mensagem: "Ponto já registrado",
+    });
+  }
+
+  const hasOneHourDifference = await db.timePunch.findFirst({
+    where: {
+      moment: {
+        gte: date.subHours(dateObject, 1),
+        lte: dateObject,
+      },
+    },
+  });
+
+  if(!!hasOneHourDifference) {
+    return res.status(400).json({
+      mensagem: "Deve haver no mínimo 1 hora de almoço",
+    });
+  }
+
   await db.timePunch.create({
     data: {
       yearMonth,
@@ -30,9 +57,26 @@ router.post("/v1/batidas", async (req: Request, res: Response) => {
     },
   });
 
-  const serialized = new PostBatidasSerializer({ momento }).serialize();
+  const momentDayPunches = await db.timePunch.findMany({
+    where: {
+      moment: {
+        gte: date.startOfDay(dateObject),
+        lte: date.endOfDay(dateObject),
+      },
+    },
+    orderBy: {
+      moment: "asc",
+    },
+  });
 
-  res.json(serialized);
+  const pontos = momentDayPunches.map((punch) =>
+    date.format(punch.moment, "HH:mm:ss")
+  );
+
+  res.json({
+    dia: date.format(dateObject, "yyyy-MM-dd"),
+    pontos,
+  });
 });
 
 export { router };
