c7be0b58ae1506e1680504eb7f6f492111848207
hugoarmor <hugoarmoreira@hotmail.com>
2024-02-11 18:04:54 -0300

    update: time-puch service create types

--- src/resources/time-punch/service.contract.ts
+++ src/resources/time-punch/service.contract.ts
@@ -1,7 +1,7 @@
 import { TimePunch } from "@prisma/client";
 
 export interface TimePunchServiceContract {
-  create(data: TimePunch): Promise<CreateTimePunchPayload>
+  create(data: CreateTimePunchPayload): Promise<TimePunch>
   getOneFromHourAgo(moment: Date): Promise<TimePunch | null>
   getDailyPunches(day: Date, options: OptionsGetDailyPunches): Promise<TimePunch[]>
   getDailyPunchesCount(day: Date): Promise<number>
