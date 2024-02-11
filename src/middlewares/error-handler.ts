78cd9ad540250424ab78d1fe3240357a3a3703b0
hugoarmor <hugoarmoreira@hotmail.com>
2024-02-11 18:03:28 -0300

    refactor: remove error handler as specific express middleware

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,8 +1,10 @@
 import { NextFunction, Request, Response } from "express";
 import { HttpError } from "../errors/http-error";
 
-export function errorHandler(err: HttpError, req: Request, res: Response, _: NextFunction) {
+export function handleError(err: HttpError, _req: Request, res: Response, next: NextFunction) {
   const statusCode = err.status || 500;
   const message = err.message || 'Internal Server Error';
   res.status(statusCode).json({ error: message });
+
+  next();
 }
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import { router } from "./routes/post.batidas";
-import { errorHandler } from "./middlewares/error-handler";
 
 const PORT = 3000;
 
@@ -9,7 +8,5 @@ const app = express();
 app.use(express.json());
 
 app.use(router);
-
-app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server listening at ${PORT}`));
