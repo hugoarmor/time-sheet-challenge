3289dc6963872a0144f306d511be43580449e769
hugoarmor <hugoarmoreira@hotmail.com>
2024-02-12 10:24:07 -0300

    refactor: error-handler message key

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -4,7 +4,7 @@ import { HttpError } from "../errors/http-error";
 export function handleError(err: HttpError, _req: Request, res: Response, next: NextFunction) {
   const statusCode = err.status || 500;
   const message = err.message || 'Internal Server Error';
-  res.status(statusCode).json({ error: message });
+  res.status(statusCode).json({ mensagem: message });
 
   next();
 }
