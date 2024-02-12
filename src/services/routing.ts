4a1346d64d6e4c88ed77ba2d740d765de4b2f221
hugoarmor <hugoarmoreira@hotmail.com>
2024-02-12 10:23:31 -0300

    add: routing service for dynamic route declaring

---
+++ src/services/routing.ts
@@ -0,0 +1,57 @@
+import { Router } from "express";
+import { container } from "tsyringe";
+import { handleError } from "../middlewares/error-handler";
+import { HttpError } from "../errors/http-error";
+
+type HttpRoute = {
+  path: string;
+  handler: string;
+  method: "get" | "post";
+  controller: string;
+};
+
+export const httpRoutes: HttpRoute[] = [];
+
+export function Get(path: string) {
+  return function (target: any, key: string, descriptor: PropertyDescriptor) {
+    httpRoutes.push({
+      path,
+      handler: key,
+      method: "get",
+      controller: target.constructor.name,
+    });
+
+    return descriptor;
+  };
+}
+
+export function Post(path: string) {
+  return function (target: any, key: string, descriptor: PropertyDescriptor) {
+    httpRoutes.push({
+      path,
+      handler: key,
+      method: "post",
+      controller: target.constructor.name,
+    });
+
+    return descriptor;
+  };
+}
+
+export function buildRoutes(httpRoutes: HttpRoute[]) {
+  const router = Router();
+
+  for (const handler of httpRoutes) {
+    const controller: any = container.resolve(handler.controller);
+
+    router[handler.method](handler.path, (req, res, next) =>
+      controller[handler.handler].call(controller, req, res).catch(
+        (error: unknown) => {
+          handleError(error as HttpError, req, res, next);
+        }
+      )
+    );
+  }
+
+  return router;
+}
