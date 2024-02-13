ebe7c2ea0d30d5ece4567b9fee8a061d7b4163ce
hugoarmor <hugoarmoreira@hotmail.com>
2024-02-13 11:25:31 -0300

    update: time-punch policy structure

--- src/resources/time-punch/policy.ts
+++ src/resources/time-punch/policy.ts
@@ -1,22 +1,40 @@
-import { format } from "date-fns";
+import { differenceInMinutes, format } from "date-fns";
 import { HttpError } from "../../errors/http-error";
 import { injectable } from "tsyringe";
 
 @injectable()
 export class TimePunchPolicy {
-  FORBIDDEN_DAYS = ["saturday", "sunday"]
+  static FORBIDDEN_DAYS = ["saturday", "sunday"];
+  static LUNCH_BREAK_MINIMUM_MINUTES = 60;
+  static TIME_PUNCHES_DAILY_LIMIT = 4;
 
-  async isLunchBreakMinimumReached(timePunchFromLessThanHourAgo?: Date) {
-    if (!!timePunchFromLessThanHourAgo) {
+  static isLunchBreakMinimumReached(moments: Date[]) {
+    if (moments.length < 2) {
+      return;
+    }
+
+    moments.sort((a, b) => a.getTime() - b.getTime());
+
+    const lunchBreak = Math.abs(
+      differenceInMinutes(
+        moments[2],
+        moments[1]
+      )
+    );
+
+    if (lunchBreak < TimePunchPolicy.LUNCH_BREAK_MINIMUM_MINUTES) {
       throw new HttpError("Deve haver no mínimo 1 hora de almoço", 400);
     }
   }
 
-  async isNotWeekendDay(moment: Date){
+  static isNotWeekendDay(moment: Date) {
     const day = format(moment, "EEEE").toLocaleLowerCase();
 
-    if (this.FORBIDDEN_DAYS.includes(day)) {
-      throw new HttpError("Sábado e domingo não são permitidos como dia de trabalho", 400);
+    if (TimePunchPolicy.FORBIDDEN_DAYS.includes(day)) {
+      throw new HttpError(
+        "Sábado e domingo não são permitidos como dia de trabalho",
+        400
+      );
     }
   }
 }
