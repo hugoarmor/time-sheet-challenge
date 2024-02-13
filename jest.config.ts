3d4451b177309472a97e3934ea472c460dc36d46
hugoarmor <hugoarmoreira@hotmail.com>
2024-02-13 11:42:30 -0300

    add: jest module

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  roots: ['<rootDir>/src'],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts$',
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['src/**/*.{ts,js}', '!src/**/*.d.ts'],
+};
+
+export default config;
